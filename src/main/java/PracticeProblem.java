ca025e889f75f8cc204982b34fc89b5a332893e4
Nithin Alagar <343954178@tdsb.ca>
2026-03-25 14:27:36 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,7 +2,7 @@
  * File: If statements
  * Author: Nithin.A
  * Date Created: Mar 23, 2026
- * Date Last Modified: Mar 24, 2026
+ * Date Last Modified: Mar 25, 2026
  */
 public class PracticeProblem {
 
@@ -11,36 +11,36 @@ public class PracticeProblem {
 	
 	
 
-	public static String evenOrOdd(int num){
-	if (0 == num%2) {
-	return "Even";
-	}
-	return "Odd";
-	}
+    public String evenOrOdd(int num){
+	    if (0 == num%2) {
+	        return "Even";
+	    }
+	    return "Odd";
+	    }
 
 	
 
-public static String teacherOrStudent(String name){
-    if ("Kalisz".equals(name)){
-        return "Teacher";
-    }
-    return "Student";
-}
-
-    
-public static int fartherFromZero(int num){
-    if (num>0){
-        num = num + 5;
-        return num;
-    }
-    if (num < 0){
-        if (num = 0){
-            return 0;
+    public String teacherOrStudent(String name){
+        if ("Kalisz".equals(name)){
+            return "Teacher";
         }
-    	num = num - 5;
-	return num;
+        return "Student";
     }
-}
-
-}
-}
+    
+        
+    public int fartherFromZero(int num){
+        if (num>0){
+            num = num + 5;
+            return num;
+        }
+        if (num < 0){
+            if (num = 0){
+                return 0;
+            }
+        	num = num - 5;
+    	return num;
+        }
+    }
+    
+    }
+    }
